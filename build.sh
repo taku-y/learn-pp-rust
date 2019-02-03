f4247cf5961802cfea9e83f1bd8358207582c3a8
taku-y <taku.yoshioka.4096@gmail.com>
2019-02-03 18:07:16 +0900

    Change directory structure

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t learn-pp-rust .
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run -it --rm \
-    --volume $(pwd)/workspace:/root/workspace \
-    --name learn-pp-rust learn-pp-rust
