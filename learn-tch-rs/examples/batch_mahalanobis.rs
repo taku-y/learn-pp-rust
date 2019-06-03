65ac141787250335b144090a54f46f5671d53dd3
taku-y <taku.yoshioka.4096@gmail.com>
2019-06-04 08:35:56 +0900

    Update test_log_prob, find a bug when writing to npz?

--- learn-tch-rs/examples/batch_mahalanobis.rs
+++ learn-tch-rs/examples/batch_mahalanobis.rs
@@ -30,14 +30,14 @@ fn _test_log_prob() {
     let scale = Tensor::of_slice(&[1.0f32, 0.0, -0.5, 2.0]).reshape(&[2, 2]);
     let dist = MultivariateNormal::new(loc, Scale::ScaleTril(scale));
 
-    let s = Tensor::arange2(-2.0, 2.0, 0.1, kind::FLOAT_CPU);
+    let s = Tensor::arange2(-8.0, 8.0, 0.05, kind::FLOAT_CPU);
     let xs = s.reshape(&[-1, 1]).ones_like().matmul(&s.reshape(&[1, -1]));
-    let ys = xs.transpose(0, 1);
+    // let ys = xs.transpose(1, 0); # it causes bug when writing it to npz?
+    let ys = s.reshape(&[-1, 1]).matmul(&s.reshape(&[1, -1]).ones_like());
     let xys = Tensor::stack(&[&xs.reshape(&[-1]), &ys.reshape(&[-1])], 1);
     let lp = dist.log_prob(&xys).reshape(&xs.size());
-
     let f = "test_log_prob.npz";
-    Tensor::write_npz(&[("xs", xs), ("ys", ys), ("lp", lp)], f);
+    Tensor::write_npz(&[("xs", &xs), ("ys", &ys), ("lp", &lp)], f);
 }
 
 fn _batch_mv(bmat: &Tensor, bvec: &Tensor) -> Tensor {
